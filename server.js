d11f2b7f1fdd1be973856334d8d83f042b713460
qthwng01 <qthwng.01@gmail.com>
2025-03-14 23:04:41 +0700

    update API Refresh, fix middlware

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ const authenticateToken = (req, res, next) => {
   if (!token) return res.status(401).json({ error: 'Access token missing' })
 
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-    if (err) return res.status(403).json({ error: 'Invalid access token' })
+    if (err) return res.status(401).json({ error: 'Invalid access token' })
     req.user = user
     next()
   })
